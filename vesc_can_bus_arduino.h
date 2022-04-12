ee7cca8708463ff6b1f6e970bd90e5ab12cc72c9
CRAIG GAULT <craig@intra-drive.com>
2022-04-12 13:56:13 +0100

    header file

---
+++ vesc_can_bus_arduino.h
@@ -0,0 +1,33 @@
+#include <Arduino.h> //required for PD# definitions
+
+class CAN
+{
+
+public:
+
+#define CAN0_INT 21                              // Set INT to pin 21
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];// Array to store serial string
+
+
+float inpVoltage, dutyCycleNow, avgInputCurrent, avgMotorCurrent, tempFET, tempMotor;
+long erpm;
+
+
+void initialize();
+void spin();
+void get_frame(); // populates rxId and rxBuf with latest can frame 
+void can_send(byte data[8]); //transmits the send commands to the sensor
+void print_raw_can_data(); //output raw can data to terminal (debug)
+
+
+void vesc_set_duty(float duty);
+void vesc_set_current(float current);
+void vesc_set_erpm(float erpm);
+float process_data_frame_vesc(char datatype, unsigned char byte1, unsigned char byte2);
+int hex2int(char buf[]);
+
+};
