f9c751e78c607ed0ffb165aaff1e5b5a16b68f19
CRAIG GAULT <craig@intra-drive.com>
2022-04-12 13:38:26 +0100

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+**VESC library for CAN bus control using MCP2515 SPI Driver**
+
+![image](https://github.com/craigg96/vesc_can_bus_arduino/blob/main/images/header.png?raw=true "Header")
+
+This repo contains the CAN bus protocol for VESC that can be used with arduino and the MCP2515 driver library by Cory Fowler (see credits).
+The MCP2515 is the most common way to integrate can bus onto the arduino platform. The VESC platform is the gold standard for brushless motor control for skateboards and ebikes. This library allows you to use the can bus instead of UART (see solidgeek vescuart library) to communicate with the VESC which is more reliable and allows the single UART port on most arduinos to be free for debugging.  
+
+## Realtime Data Messages
+|     Parameter     |
+|:-----------------:|
+|        rpm        |
+|     inpVoltage    |
+|    dutyCycleNow   |
+|  avgInputCurrent  |
+|  avgMotorCurrent  |
+|      tempFET      |
+|     tempMotor     |
+
+## Command Messages
+|     Parameter     |
+|:-----------------:|
+|        erpm       |
+|      dutycycle    |
+|       current     |
+
+To use the library out of the box, the vesc tool must be configured as shown in images/vesc_tool_app_settings.png.
+VESC ID = 10
+CAN STATUS_MESSAGE_MODE = CAN_STATUS_1_2_3_4_5
+CAN Baud Rate = CAN_BAUD_250K
+
+# Credits
+
+* Craig Gault - https://github.com/craigg96
+
+DEPENDENCIES
+
+* https://github.com/coryjfowler/MCP_CAN_lib
+
---
+++ vesc_can_bus_arduino.cpp
@@ -0,0 +1,114 @@
+#include <mcp_can.h>
+#include <SPI.h>
+
+MCP_CAN CAN0(10);                               // Set CS to pin 10
+
+void CAN::initialize() {
+  CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ);
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+  //SET PINMODE DONE IN MAIN // pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+}
+
+void CAN::spin() {
+  get_frame();
+
+  if (rxId == 0x8000090A) { //
+    dutyCycleNow = process_data_frame_vesc('D', rxBuf[6], rxBuf[7]);
+    avgMotorCurrent = process_data_frame_vesc('C', rxBuf[4], rxBuf[5]);
+    unsigned char erpmvals[4];
+    erpmvals[0] = rxBuf[3];
+    erpmvals[1] = rxBuf[2];
+    erpmvals[2] = rxBuf[1];
+    erpmvals[3] = rxBuf[0];
+    erpm = *(long *)erpmvals;
+
+    //need to add in the rpm conversion function for 4 byte values
+  }
+  if (rxId == 0x8000100A) { //
+    tempFET = process_data_frame_vesc('F', rxBuf[0], rxBuf[1]);
+    tempMotor = process_data_frame_vesc('T', rxBuf[2], rxBuf[3]);
+    avgInputCurrent = process_data_frame_vesc('I', rxBuf[4], rxBuf[5]);
+  }
+  if (rxId == 0x80001B0A) {
+    char receivedByte[4], *p;
+    sprintf(receivedByte, "%02X%02X", rxBuf[4], rxBuf[5]);
+    inpVoltage = hex2int(receivedByte) * 0.1;
+  }
+
+}
+
+void CAN::print_raw_can_data() {
+  int len = 8;
+  sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+  Serial.print(msgString);
+  for (byte i = 0; i < len; i++) {
+    sprintf(msgString, " 0x%.2X", rxBuf[i]);
+    Serial.print(msgString);
+  }
+  Serial.println();
+}
+
+float CAN::process_data_frame_vesc(char datatype, unsigned char byte1, unsigned char byte2) {
+  char receivedByte[4], *p;
+  sprintf(receivedByte, "%02X%02X", byte1, byte2);
+  float output = hex2int(receivedByte);
+
+  switch (datatype) {
+    case 'D': output *= 0.001; break; //dutyCycleNow
+    case 'C': output *= 0.1; break; //avgMotorCurrent
+    case 'F': output *= 0.1; break; //tempFET
+    case 'T': output *= 0.1; break; //tempMotor
+    case 'I': output *= 0.1; break; //avgInputCurrent
+    case 'V': output *= 0.1; break; //inpVoltage
+  }
+  return output;
+}
+
+int CAN::hex2int(char buf[])
+{
+  //  if (size(buf) == 2){
+  return (short) strtol(buf, NULL, 16);
+  //  }
+  //  if (size(buf) == 4){
+  //    return (int) strtol(buf, NULL, 16);
+  //  }
+}
+
+void CAN::vesc_set_duty(float duty) {
+  uint32_t set_value = duty * 100000;
+  uint8_t buffer[4];
+  buffer[0] = (set_value >> 24) & 0xFF;
+  buffer[1] = (set_value >> 16) & 0xFF;
+  buffer[2] = (set_value  >> 8  )  & 0xFF;
+  buffer[3] = set_value & 0xFF;
+  byte sndStat = CAN0.sendMsgBuf(0x0000000A, 1, 4, buffer);
+}
+
+void CAN::vesc_set_current(float current) {
+  uint32_t set_value;
+  if (AVG_CADENCE < -20 && motor_estop == false) {//AVG_TORQUE > 0.05 &&
+    set_value = current * 1000 * ASSISTANCE_TORQUE_MULTIPLIER;
+  }
+  else {
+    set_value = 0;
+  }
+  uint8_t buffer[4];
+  buffer[0] = (set_value >> 24) & 0xFF;
+  buffer[1] = (set_value >> 16) & 0xFF;
+  buffer[2] = (set_value  >> 8  )  & 0xFF;
+  buffer[3] = set_value & 0xFF;
+  byte sndStat = CAN0.sendMsgBuf(0x0000010A, 1, 4, buffer);
+}
+
+void CAN::vesc_set_erpm(float erpm) {
+  uint32_t set_value = erpm;
+  uint8_t buffer[4];
+  buffer[0] = (set_value >> 24) & 0xFF;
+  buffer[1] = (set_value >> 16) & 0xFF;
+  buffer[2] = (set_value  >> 8  )  & 0xFF;
+  buffer[3] = set_value & 0xFF;
+  byte sndStat = CAN0.sendMsgBuf(0x0000030A, 1, 4, buffer);
+}
+void CAN::get_frame() {
+  CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+}
